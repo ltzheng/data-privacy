4cea2330f1e29e16a278bbba95077526fa070388
ltzheng <zlt0116@mail.ustc.edu.cn>
2021-07-09 14:43:50 +0800

    update paillier test with gmpy2

--- experiments/federated_learning/paillier_test.py
+++ experiments/federated_learning/paillier_test.py
@@ -1,6 +1,7 @@
 import random, sys 
 import time
-from gmpy2 import mpz, powmod, invert, is_prime, random_state, mpz_urandomb, rint_round, log2, gcd 
+from gmpy2 import mpz, powmod, invert, is_prime, random_state, mpz_urandomb, rint_round, rint_floor, log2, gcd 
+from termcolor import colored
 
 rand = random_state(random.randrange(sys.maxsize))
 
@@ -41,103 +42,91 @@ def generate_keypair(bits):
     return PrivateKey(p, q, n), PublicKey(n)
 
 def enc(pub, plain):
-    """
-        parameters: public key, plaintext
-    """
+    """Parameters: public key, plaintext"""
+
     def generate_r(n):
         """generate a random number s.t. gcd(r, n) = 1"""    
         while True:
-            r = mpz(random.randint(1, n-1))
+            r = mpz(random.randint(1, n - 1))
             if gcd(r, n) == 1:
                 return r
+    
+    # Mathematically:
     # (a * b) mod c = (a mod c * b mod c) mod c
     # c = (g^m * r^n) mod n^2 = (g^m mod n^2 * r^n mod n^2) mod n^2
-    g, n = pub.g, pub.n
+    g, n, n_sq = pub.g, pub.n, pub.n_sq
     r = generate_r(n)
-    cipher = powmod(powmod(g, plain, n**2) * powmod(r, n, n**2), 1, n**2)
+    cipher = powmod(powmod(g, plain, n_sq) * powmod(r, n, n_sq), 1, n_sq)
     return cipher
 
 def dec(priv, pub, cipher):
-    """
-        parameters: private key, public key, cipher
-    """
-    n = pub.n
-    x = powmod(cipher, priv.l, n**2)
-    L = mpz(rint_round((x - 1) / n) - 1)
-    plain = powmod(L * priv.m, 1, n)
+    """Parameters: private key, public key, cipher"""
+    n, n_sq = pub.n, pub.n_sq
+    x = powmod(cipher, priv.l, n_sq)
+    L = rint_floor((x - 1) // n)
+    plain = powmod(mpz(L * priv.m), 1, n)
     return plain
 
 def enc_add(pub, m1, m2):
-    """add one encrypted integer to another"""
-    return powmod(enc(pub, m1) * enc(pub, m2), 1, pub.n**2)
+    """Add one encrypted integer to another"""
+    return powmod(m1 * m2, 1, pub.n_sq)
 
 def enc_add_const(pub, m, c):
-    """add a constant to an encrypted integer"""
-    return powmod(enc(pub, m) * powmod(pub.g, c, 1), 1, pub.n**2)
+    """Add a constant to an encrypted integer"""
+    n_sq = pub.n_sq
+    return powmod(powmod(m, 1, n_sq) * powmod(pub.g, c, n_sq), 1, n_sq)
 
 def enc_mul_const(pub, m, c):
-    """multiply an encrypted integer by a constant"""
-    return powmod(enc(pub, m), c, pub.n**2)
+    """Multiply an encrypted integer by a constant"""
+    return powmod(m, c, pub.n_sq)
 
-def test_enc_add(bit_len, priv, pub):
+def test(mode, bit_len, priv, pub):
+    def generate_num(bit_len):
+        return mpz(2)**(bit_len - 1) + mpz_urandomb(rand, bit_len - 1)
+    
     elapsed_times = {}
-    print('=====test enc_add=====')
-    m1, m2 = mpz_urandomb(rand, bit_len - 1), mpz_urandomb(rand, bit_len - 1)
+    print('=====TEST ' + mode + '=====')
+    a = generate_num(bit_len)
+    b = generate_num(bit_len)
+    c = generate_num(bit_len)
+    m1 = enc(pub, a)
+    m2 = enc(pub, b)
+
     enc_start = time.time()
-    enc_plain = enc_add(pub, m1, m2)
+    
+    if mode == 'enc_add':
+        enc_plain = enc_add(pub, m1, m2)
+        ground_truth = a + b
+    elif mode == 'enc_add_const':
+        enc_plain = enc_add_const(pub, m1, c)
+        ground_truth = a + c
+    elif mode == 'enc_mul_const':
+        enc_plain = enc_mul_const(pub, m1, c)
+        ground_truth = a * c
+    else:
+        raise NotImplementedError
+
     enc_end = time.time()
     dec_start = time.time()
     dec_cipher = dec(priv, pub, enc_plain)
     dec_end = time.time()
-    print('decrypted:', dec_cipher)
-    print('ground truth:', powmod(m1 + m2, 1, pub.n))
+    if dec_cipher == ground_truth:
+        print(colored('PASS', 'green'))
+    else:
+        print(colored('FAIL', 'red'))
+    
     elapsed_times['enc'] = enc_end - enc_start
     elapsed_times['dec'] = dec_end - dec_start
     print('elapsed time of encryption:', elapsed_times['enc'])
     print('elapsed time of decryption:', elapsed_times['dec'])
     return elapsed_times
 
-def test_enc_add_const(bit_len, priv, pub):
-    elapsed_times = {}
-    print('=====test enc_add_const=====')
-    m1, c = mpz_urandomb(rand, bit_len - 1), mpz_urandomb(rand, bit_len - 1)
-    enc_start = time.time()
-    enc_plain = enc_add_const(pub, m1, c)
-    enc_end = time.time()
-    dec_start = time.time()
-    dec_cipher = dec(priv, pub, enc_plain)
-    dec_end = time.time()
-    print('decrypted:', dec_cipher)
-    print('ground truth:', powmod(m1 + c, 1, pub.n))
-    elapsed_times['enc'] = enc_end - enc_start
-    elapsed_times['dec'] = dec_end - dec_start
-    print('elapsed time of encryption:', elapsed_times['enc'])
-    print('elapsed time of decryption:', elapsed_times['dec'])
-    return elapsed_times
-
-def test_enc_mul_const(bit_len, priv, pub):
-    elapsed_times = {}
-    print('=====test enc_mul_const=====')
-    m1, k = mpz_urandomb(rand, bit_len - 1), mpz_urandomb(rand, bit_len - 1)
-    enc_start = time.time()
-    enc_plain = enc_mul_const(pub, m1, k)
-    enc_end = time.time()
-    dec_start = time.time()
-    dec_cipher = dec(priv, pub, enc_plain)
-    dec_end = time.time()
-    print('decrypted:', dec_cipher)
-    print('ground truth:', powmod(k * m1, 1, pub.n))
-    elapsed_times['enc'] = enc_end - enc_start
-    elapsed_times['dec'] = dec_end - dec_start
-    print('elapsed time of encryption:', elapsed_times['enc'])
-    print('elapsed time of decryption:', elapsed_times['dec'])
-    return elapsed_times
 
 if __name__ == '__main__':
     priv, pub = generate_keypair(1024)
     for bit_len in range(10, 1000, 10):
         elapsed_times = {}
-        elapsed_times['enc_add'] = test_enc_add(bit_len, priv, pub)
-        elapsed_times['enc_add_const'] = test_enc_add_const(bit_len, priv, pub)
-        elapsed_times['enc_mul_const'] = test_enc_mul_const(bit_len, priv, pub)
+        elapsed_times['enc_add'] = test('enc_add', bit_len, priv, pub)
+        elapsed_times['enc_add_const'] = test('enc_add_const', bit_len, priv, pub)
+        elapsed_times['enc_mul_const'] = test('enc_mul_const', bit_len, priv, pub)
         print(elapsed_times)
